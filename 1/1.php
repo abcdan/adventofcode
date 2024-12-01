ac13de5db8aaa77f88479e9347864e0c91789dd4
Daniël <15849319+abcdan@users.noreply.github.com>
2024-12-01 18:02:37 +0100

    1.php

--- 1/1.php
+++ 1/1.php
@@ -4,8 +4,8 @@ $contents = file_get_contents(filename: '1.txt');
 $left  = [];
 $right = [];
 
-foreach (explode("\n", $contents) as $line) {
-    list($left[], $right[]) = explode('   ', $line);
+foreach (explode(separator: "\n", string: $contents) as $line) {
+    list($left[], $right[]) = explode(separator: '   ', string: $line);
 }
 
 sort(array:$left);
@@ -14,7 +14,7 @@ sort(array:$right);
 $diff = 0;
 
 foreach ($left as $index => $l) {
-    $diff = $diff + abs($l - $right[$index]);
+    $diff = $diff + abs(num: $l - $right[$index]);
 }
 
 echo $diff;
